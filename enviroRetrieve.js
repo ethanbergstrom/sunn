b515eb2ec2d97a79147758fb39ff15452391c076
Ethan <ethan@arios.lan>
2019-03-17 11:06:10 -0500

    Try putting query on one line

--- enviroRetrieve.js
+++ enviroRetrieve.js
@@ -1,7 +1,7 @@
 var sdb = require('aws-sdb-promise');
 var headers = require('corsHeaders');
 
-const allowedAttributes = ['temperature','accelerometer', 'heading', 'lux','pressure','rgb'];
+const allowedAttributes = ['temperature','accelerometer','heading','lux','pressure','rgb'];
 const maxDaySpan = 31;
 
 // Implement retrieve logic
@@ -13,13 +13,7 @@ async function retrieveImpl(event) {
                 headers: headers,
                 body: await sdb.select(
                     {
-                        SelectExpression: `
-                        SELECT 
-                            collectedAt, 
-                            ${event.attribute} 
-                        FROM ${process.env.SDB_DOMAIN} 
-                        WHERE collectedAt > '${new Date(new Date().setDate(new Date().getDate()-event.daySpan)).toISOString()}'
-                        `
+                        SelectExpression: `SELECT collectedAt, ${event.attribute} FROM ${process.env.SDB_DOMAIN} WHERE collectedAt > '${new Date(new Date().setDate(new Date().getDate()-event.daySpan)).toISOString()}'`
                     }
                 )
             };
@@ -28,7 +22,7 @@ async function retrieveImpl(event) {
     catch(err) {
         console.log(JSON.stringify(err,undefined,2));
         return {
-            statusCode: 500,
+            statusCode: err.statusCode,
             headers: headers,
             body: err
         };
