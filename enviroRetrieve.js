e44202a398d208b2d0a722536e6a8ab6f5386555
Ethan <ethan@arios.lan>
2019-03-17 12:09:14 -0500

    Only pull back the last hour

--- enviroRetrieve.js
+++ enviroRetrieve.js
@@ -2,21 +2,21 @@ var sdb = require('aws-sdb-promise');
 var headers = require('corsHeaders');
 
 const allowedAttributes = ['temperature','accelerometer','heading','lux','pressure','rgb'];
-const maxDaySpan = 31;
+const minuteSpan = 60;
+const msPerMinute = 60000;
+const timeSpan = minuteSpan * msPerMinute;
 
 // Implement retrieve logic
 async function retrieveImpl(event) {
     try {
-        if(allowedAttributes.includes(event.attribute) && event.daySpan <= maxDaySpan) {
-            console.log('attribute =', event.attribute);
-            console.log('lookback =', (new Date(new Date().setDate(new Date().getDate()-event.daySpan)).toISOString()));
-            console.log('query =', `SELECT collectedAt, ${event.attribute} FROM \`${process.env.SDB_DOMAIN}\` WHERE collectedAt > '${new Date(new Date().setDate(new Date().getDate()-event.daySpan)).toISOString()}'`);
+        if(allowedAttributes.includes(event.attribute)) {
+            console.log('query =', `SELECT collectedAt, ${event.attribute} FROM \`${process.env.SDB_DOMAIN}\` WHERE collectedAt > '${new Date(new Date() - timeSpan).toISOString()}'`);
             return {
                 statusCode: 200,
                 headers: headers,
                 body: await sdb.select(
                     {
-                        SelectExpression: `SELECT collectedAt, ${event.attribute} FROM \`${process.env.SDB_DOMAIN}\` WHERE collectedAt > '${new Date(new Date().setDate(new Date().getDate()-event.daySpan)).toISOString()}'`
+                        SelectExpression: `SELECT collectedAt, ${event.attribute} FROM \`${process.env.SDB_DOMAIN}\` WHERE collectedAt > '${new Date(new Date() - timeSpan).toISOString()}'`
                     }
                 )
             };
