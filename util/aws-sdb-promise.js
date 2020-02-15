e4b2cdb68bfe5da4c53cf827a5238a8d5bc83b16
Ethan Bergstrom <ethan.bergstrom@gmail.com>
2020-02-15 12:25:00 -0600

    Trying out converting SDB attrs upstream

--- util/aws-sdb-promise.js
+++ util/aws-sdb-promise.js
@@ -3,6 +3,19 @@ var AWS = require('aws-sdk');
 AWS.config.update({ region: process.env.SDB_REGION });
 sdb = new AWS.SimpleDB();
 
+async function attributeToObjects(pairs) {
+    var attributes = {};
+
+    pairs.forEach(function(aPair) {
+        if (!attributes[aPair.Name]) {
+            attributes[aPair.Name] = aPair.Value;
+        }
+        attributes[aPair.Name].push(aPair.Value);
+    });
+
+    return attributes;
+}
+
 // Create Promise wrappers
 const putAttributes = params => new Promise(
     (resolve,reject) => {
@@ -17,7 +30,14 @@ const select = params => new Promise(
     (resolve,reject) => {
         sdb.select(params, function(err, data) {
             if (err) reject(err);
-            else resolve(data);
+            else {
+                // Convert SDBs attributes into Javascript objects
+                var items = [];
+                items = data.Items.map(function(anItem) {
+                    return attributeToObjects(anItem.Attributes);
+                })
+                resolve(items);
+            }
         });
     }
 );
