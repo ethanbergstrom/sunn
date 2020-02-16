8ffc90ec48cab7bb5531fee2c16726891d84da0f
Ethan Bergstrom <ethan.bergstrom@gmail.com>
2020-02-15 18:02:09 -0600

    Add multi-attribute support

--- enviroRetrieve.js
+++ enviroRetrieve.js
@@ -9,20 +9,22 @@ const timeSpan = minuteSpan * msPerMinute;
 // Implement retrieve logic
 async function retrieveImpl(event) {
 	try {
-		if(allowedAttributes.includes(event.attribute)) {
-			console.log('query =', `SELECT collectedAt, ${event.attribute} FROM \`${process.env.SDB_DOMAIN}\` WHERE collectedAt > '${new Date(new Date() - timeSpan).toISOString()}'`);
+		const enviroAttributes = event.attributes.filter(value => allowedAttributes.includes(value));
+		if (Array.isArray(enviroAttributes) && enviroAttributes.length) {
+			console.log('query =', `SELECT collectedAt,${enviroAttributes} FROM \`${process.env.SDB_DOMAIN}\` WHERE collectedAt > '${new Date(new Date() - timeSpan).toISOString()}'`);
 			return {
 				statusCode: 200,
 				headers: headers,
 				body: await sdb.select(
 					{
-						SelectExpression: `SELECT collectedAt, ${event.attribute} FROM \`${process.env.SDB_DOMAIN}\` WHERE collectedAt > '${new Date(new Date() - timeSpan).toISOString()}'`
+						SelectExpression: `SELECT collectedAt,${enviroAttributes} FROM \`${process.env.SDB_DOMAIN}\` WHERE collectedAt > '${new Date(new Date() - timeSpan).toISOString()}'`
 					}
 				)
 			};
+		} else {
+			throw `Invalid attributes: ${event.attributes}`
 		}
-	}
-	catch(err) {
+	} catch(err) {
 		console.log(JSON.stringify(err,undefined,2));
 		return {
 			statusCode: err.statusCode,
