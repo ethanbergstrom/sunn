867aeb248c0f175680012b14944a846b834c8bbc
Ethan Bergstrom <13603162+ethanbergstrom@users.noreply.github.com>
2023-01-15 18:24:25 +0000

    Fix DDL syntax

--- oci/modules/oci-nosql/main.tf
+++ oci/modules/oci-nosql/main.tf
@@ -1,7 +1,7 @@
 resource "oci_nosql_table" "database" {
     compartment_id = var.compartment_ocid
     name = "database"
-    ddl_statement = "CREATE TABLE enviro (id INTEGER GENERATED ALWAYS AS IDENTITY, createdAt AS TIMESTAMP, heading AS FLOAT, accelerometer AS STRING, temperature AS FLOAT, pressure AS FLOAT, rgb AS STRING, lux AS INTEGER, KEY (id));"
+    ddl_statement = "CREATE TABLE enviro (id INTEGER GENERATED ALWAYS AS IDENTITY, createdAt TIMESTAMP, heading FLOAT, accelerometer STRING, temperature FLOAT, pressure FLOAT, rgb STRING, lux INTEGER, KEY (id));"
     table_limits {
         max_read_units = "25"
         max_write_units = "25"
