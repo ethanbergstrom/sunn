62b2ea9610ad233929cb8db8a19e38cb105f0ebc
Ethan Bergstrom <13603162+ethanbergstrom@users.noreply.github.com>
2023-06-11 14:00:24 +0000

    Remove semicolon from DDL

--- oci/modules/oci-nosql/main.tf
+++ oci/modules/oci-nosql/main.tf
@@ -4,7 +4,7 @@ resource "oci_nosql_table" "database" {
     # Examples: https://docs.oracle.com/en/database/other-databases/nosql-database/22.3/java-driver-table/create-table.html
     # Key design: https://docs.oracle.com/en/database/other-databases/nosql-database/22.3/java-driver-table/primary-keys.html
     # Index design: https://docs.oracle.com/en/database/other-databases/nosql-database/22.3/java-driver-table/creating-indexes.html
-    ddl_statement = "CREATE TABLE IF NOT EXISTS enviro(createdAt TIMESTAMP, heading FLOAT, accelerometer STRING, temperature FLOAT, pressure FLOAT, rgb STRING, lux INTEGER, PRIMARY KEY (createdAt));"
+    ddl_statement = "CREATE TABLE IF NOT EXISTS enviro(createdAt TIMESTAMP, heading FLOAT, accelerometer STRING, temperature FLOAT, pressure FLOAT, rgb STRING, lux INTEGER, PRIMARY KEY (createdAt))"
     table_limits {
         max_read_units = "25"
         max_write_units = "25"
