8f78e775467789384c6dd72e885f47f64f5d855d
Ethan Bergstrom <13603162+ethanbergstrom@users.noreply.github.com>
2023-06-11 13:58:05 +0000

    Trying some stuff

--- oci/modules/oci-nosql/main.tf
+++ oci/modules/oci-nosql/main.tf
@@ -1,7 +1,10 @@
 resource "oci_nosql_table" "database" {
     compartment_id = var.compartment_ocid
     name = "database"
-    ddl_statement = "CREATE TABLE enviro (id INTEGER GENERATED ALWAYS AS IDENTITY, createdAt TIMESTAMP, heading FLOAT, accelerometer STRING, temperature FLOAT, pressure FLOAT, rgb STRING, lux INTEGER, KEY (id));"
+    # Examples: https://docs.oracle.com/en/database/other-databases/nosql-database/22.3/java-driver-table/create-table.html
+    # Key design: https://docs.oracle.com/en/database/other-databases/nosql-database/22.3/java-driver-table/primary-keys.html
+    # Index design: https://docs.oracle.com/en/database/other-databases/nosql-database/22.3/java-driver-table/creating-indexes.html
+    ddl_statement = "CREATE TABLE IF NOT EXISTS enviro(createdAt TIMESTAMP, heading FLOAT, accelerometer STRING, temperature FLOAT, pressure FLOAT, rgb STRING, lux INTEGER, PRIMARY KEY (createdAt));"
     table_limits {
         max_read_units = "25"
         max_write_units = "25"
