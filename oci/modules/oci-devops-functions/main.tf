7403574a63736efcf064b9fc46793e59728ca73e
Ethan Bergstrom <13603162+ethanbergstrom@users.noreply.github.com>
2023-07-02 15:46:26 +0000

    Use compartment ID in policy to keep it clean

--- oci/modules/oci-devops-functions/main.tf
+++ oci/modules/oci-devops-functions/main.tf
@@ -201,14 +201,14 @@ resource "oci_identity_policy" "devopsPolicy" {
   name           = "devopsPolicy"
   # Policies require a description
   description    = "Provide the necessary permissions for the Enviro DevOps Project to complete its pipeline steps"
-  compartment_id = var.tenancy_ocid
+  compartment_id = var.compartment_ocid
 
   statements = [
-    "Allow dynamic-group ${oci_identity_dynamic_group.devopsDynGrouop.name} to manage devops-family in compartment ${var.compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.devopsDynGrouop.name} to manage functions-family in compartment ${var.compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.devopsDynGrouop.name} to use ons-topics in compartment ${var.compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.devopsDynGrouop.name} to use repos in compartment ${var.compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.devopsDynGrouop.name} to read secret-family in compartment ${var.compartment_ocid}",
+    "Allow dynamic-group ${oci_identity_dynamic_group.devopsDynGrouop.name} to manage devops-family in compartment id ${var.compartment_ocid}",
+    "Allow dynamic-group ${oci_identity_dynamic_group.devopsDynGrouop.name} to manage functions-family in compartment id ${var.compartment_ocid}",
+    "Allow dynamic-group ${oci_identity_dynamic_group.devopsDynGrouop.name} to use ons-topics in compartment id ${var.compartment_ocid}",
+    "Allow dynamic-group ${oci_identity_dynamic_group.devopsDynGrouop.name} to use repos in compartment id ${var.compartment_ocid}",
+    "Allow dynamic-group ${oci_identity_dynamic_group.devopsDynGrouop.name} to read secret-family in compartment id ${var.compartment_ocid}",
   ]
 }
 
