9d18b4624b102ce683236fdd69c341c23a1653dd
Ethan Bergstrom <13603162+ethanbergstrom@users.noreply.github.com>
2023-08-05 21:22:55 +0000

    Flesh out Retrieve function

--- functions/enviroretrieve/func.js
+++ functions/enviroretrieve/func.js
@@ -1,10 +1,22 @@
 const fdk=require('@fnproject/fdk');
+const NoSQLClient = require('oracle-nosqldb').NoSQLClient;
+const client = new NoSQLClient({
+	compartment: process.env.COMPARTMENT_OCID,
+    auth: {
+		iam: {
+			useResourcePrincipal: true
+		}
+	}
+});
 
-fdk.handle(function(input){
-  let name = "World! (you didnt specify your name, so I'm just returning a generic default.)";
-  if (input.name) {
-    name = input.name;
-  }
-  console.log('\nInside Node Hello World function')
-  return {'message': 'Hello ' + name}
+const allowedAttributes = ['temperature','lux','pressure'];
+
+fdk.handle(async function (input) { 
+	try {
+    const enviroAttributes = input.attributes.filter(value => allowedAttributes.includes(value));
+    return await client.queryIterable(`SELECT collectedAt,${enviroAttributes} FROM \`${process.env.TABLE_NAME}\` WHERE collectedAt > '${new Date(new Date() - 3600000).toISOString()}'`)
+  } catch (err) {
+		console.log(JSON.stringify(err, undefined, 2));
+		return err;
+	}
 })
