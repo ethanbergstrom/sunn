09a41901ddc57062a33f91d8c0b48201fa49c011
Ethan Bergstrom <13603162+ethanbergstrom@users.noreply.github.com>
2023-08-05 23:19:51 +0000

    Adding some logging

--- functions/enviroretrieve/func.js
+++ functions/enviroretrieve/func.js
@@ -17,6 +17,8 @@ fdk.handle(async function (input) {
 		const enviroAttributes = input.attributes.filter(value => allowedAttributes.includes(value));
 		const query = `SELECT collectedAt,${enviroAttributes} FROM ${process.env.TABLE_NAME} WHERE collectedAt > '${new Date(new Date() - 3600000).toISOString()}'`
         for await(let result of client.queryIterable(query)) {
+			console.log(result)
+			console.log(resultSet)
 			resultSet.concat(result.rows)
         }
 		return resultSet
