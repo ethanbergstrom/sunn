dd48a1209e5357823adf5534d6c9e01a1779c271
Ethan Bergstrom <13603162+ethanbergstrom@users.noreply.github.com>
2023-08-05 22:16:40 +0000

    Dont try to make Retrieve be async

--- functions/enviroretrieve/func.js
+++ functions/enviroretrieve/func.js
@@ -1,21 +1,21 @@
-const fdk=require('@fnproject/fdk');
+const fdk = require('@fnproject/fdk');
 const NoSQLClient = require('oracle-nosqldb').NoSQLClient;
 const client = new NoSQLClient({
 	compartment: process.env.COMPARTMENT_OCID,
-    auth: {
+	auth: {
 		iam: {
 			useResourcePrincipal: true
 		}
 	}
 });
 
-const allowedAttributes = ['temperature','lux','pressure'];
+const allowedAttributes = ['temperature', 'lux', 'pressure'];
 
-fdk.handle(async function (input) { 
+fdk.handle(function (input) {
 	try {
-    const enviroAttributes = input.attributes.filter(value => allowedAttributes.includes(value));
-    return await client.queryIterable(`SELECT collectedAt,${enviroAttributes} FROM \`${process.env.TABLE_NAME}\` WHERE collectedAt > '${new Date(new Date() - 3600000).toISOString()}'`)
-  } catch (err) {
+		const enviroAttributes = input.attributes.filter(value => allowedAttributes.includes(value));
+		return client.query(`SELECT collectedAt,${enviroAttributes} FROM \`${process.env.TABLE_NAME}\` WHERE collectedAt > '${new Date(new Date() - 3600000).toISOString()}'`)
+	} catch (err) {
 		console.log(JSON.stringify(err, undefined, 2));
 		return err;
 	}
