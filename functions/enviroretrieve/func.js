546d8423df0496f368eff2d28451c67fefd0301c
Ethan Bergstrom <13603162+ethanbergstrom@users.noreply.github.com>
2023-08-05 22:30:01 +0000

    Fix SQL syntax for use with O NoSQL

--- functions/enviroretrieve/func.js
+++ functions/enviroretrieve/func.js
@@ -14,7 +14,7 @@ const allowedAttributes = ['temperature', 'lux', 'pressure'];
 fdk.handle(function (input) {
 	try {
 		const enviroAttributes = input.attributes.filter(value => allowedAttributes.includes(value));
-		return client.query(`SELECT collectedAt,${enviroAttributes} FROM \`${process.env.TABLE_NAME}\` WHERE collectedAt > '${new Date(new Date() - 3600000).toISOString()}'`)
+		return client.query(`SELECT collectedAt,${enviroAttributes} FROM ${process.env.TABLE_NAME} WHERE collectedAt > '${new Date(new Date() - 3600000).toISOString()}'`)
 	} catch (err) {
 		console.log(JSON.stringify(err, undefined, 2));
 		return err;
