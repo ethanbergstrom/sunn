093995622ac77a826ff43f3f773400925eebcaff
Ethan Bergstrom <13603162+ethanbergstrom@users.noreply.github.com>
2023-08-13 17:45:07 +0000

    Fix initial function reference

--- oci/modules/oci-devops-functions/main.tf
+++ oci/modules/oci-devops-functions/main.tf
@@ -332,7 +332,7 @@ resource "oci_functions_function" "enviroStore" {
   application_id = oci_functions_application.function_application.id
   display_name   = "enviroStore"
   memory_in_mbs  = "128"
-  image = "${oci_devops_build_run.initial_build_run.build_outputs[0].delivered_artifacts[0].items[0].image_uri}:latest"
+  image = "${oci_devops_build_run.initial_build_run.build_outputs[0].delivered_artifacts[0].items[0].image_uri}"
   
   lifecycle {
     ignore_changes = [image,image_digest]
@@ -343,7 +343,7 @@ resource "oci_functions_function" "enviroRetrieve" {
   application_id = oci_functions_application.function_application.id
   display_name   = "enviroRetrieve"
   memory_in_mbs  = "128"
-  image = "${oci_devops_build_run.initial_build_run.build_outputs[0].delivered_artifacts[0].items[1].image_uri}:latest"
+  image = "${oci_devops_build_run.initial_build_run.build_outputs[0].delivered_artifacts[0].items[1].image_uri}"
   
   lifecycle {
     ignore_changes = [image,image_digest]
