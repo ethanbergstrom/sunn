3021e5167608bac2ea64c7fd9f9a2ac5591572ef
Ethan Bergstrom <13603162+ethanbergstrom@users.noreply.github.com>
2023-08-13 17:17:28 +0000

    Create user for Pi to authenticate

--- oci/modules/oci-devops-functions/main.tf
+++ oci/modules/oci-devops-functions/main.tf
@@ -427,36 +427,36 @@ resource "oci_identity_policy" environFnAppPolicy {
   ]
 }
 
-# resource "oci_identity_user" enviroRetrieveSvcUser {
-#   name           = "enviroRetrieveSvcUser"
-#   # Policies require a description
-#   description    = "Simple user with permission to invoke the enviroRetrieve function"
-#   compartment_id = var.tenancy_ocid
-# }
+resource "oci_identity_user" enviroStoreSvcUser {
+  name           = "enviroStoreSvcUser"
+  # Policies require a description
+  description    = "Simple user with permission to invoke the enviroStore function"
+  compartment_id = var.tenancy_ocid
+}
 
-# resource "oci_identity_group" enviroRetrieveSvcGroup {
-#   compartment_id = var.tenancy_ocid
-#   name           = "enviroRetrieveSvcGroup"
-#   # Dynamic groups require a description
-#   description    = "Simple group with permission to invoke the enviroRetrieve function"
-# }
+resource "oci_identity_group" enviroStoreSvcGroup {
+  compartment_id = var.tenancy_ocid
+  name           = "enviroStoreSvcGroup"
+  # Dynamic groups require a description
+  description    = "Simple group with permission to invoke the enviroStore function"
+}
 
-# resource "oci_identity_user_group_membership" "enviroRetrieveSvcGroupMember" {
-#     #Required
-#     group_id = oci_identity_group.enviroRetrieveSvcGroup.id
-#     user_id = oci_identity_user.enviroRetrieveSvcUser.id
-# }
+resource "oci_identity_user_group_membership" "enviroStoreSvcGroupMember" {
+    #Required
+    group_id = oci_identity_group.enviroStoreSvcGroup.id
+    user_id = oci_identity_user.enviroStoreSvcUser.id
+}
 
-# resource "oci_identity_policy" enviroRetrieveSvcPolicy {
-#   name           = "enviroRetrieveSvcPolicy"
-#   # Policies require a description
-#   description    = "Only allow enviroRetrieve service account access to invoke corresponding function"
-#   compartment_id = var.compartment_ocid
+resource "oci_identity_policy" enviroStoreSvcPolicy {
+  name           = "enviroStoreSvcPolicy"
+  # Policies require a description
+  description    = "Only allow enviroStore service account access to invoke corresponding function"
+  compartment_id = var.compartment_ocid
 
-#   statements = [
-#     "Allow group id ${oci_identity_group.enviroRetrieveSvcGroup.id} to use fn-invocation in compartment id ${var.compartment_ocid} where target.function.id = '${oci_functions_function.enviroRetrieve.id}'"
-#   ]
-# }
+  statements = [
+    "Allow group id ${oci_identity_group.enviroStoreSvcGroup.id} to use fn-invocation in compartment id ${var.compartment_ocid} where target.function.id = '${oci_functions_function.enviroStore.id}'"
+  ]
+}
 
 resource oci_apigateway_gateway enviroGateway {
   compartment_id = var.compartment_ocid
